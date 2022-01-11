bb374a8ef59fb6b651cda2828bb9ece07cd80a5f
Orr Shalom Dvory <dvoreader@gmail.com>
2022-01-11 20:12:20 +0200

    initial commit

---
+++ lib/wav_io.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library wav_io;
+
+export 'src/wav_io_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
